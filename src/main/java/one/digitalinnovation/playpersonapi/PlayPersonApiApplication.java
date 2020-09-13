5db96a64014b0297e3502a65dba9d341a588a447
alexandre_lalaina <alexandre_lalaina@yahoo.com.br>
2020-09-13 19:22:06 -0300

    Primeiro commit

---
+++ src/main/java/one/digitalinnovation/playpersonapi/PlayPersonApiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.playpersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayPersonApiApplication.class, args);
+	}
+
+}
